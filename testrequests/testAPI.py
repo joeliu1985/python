8cdefffcac9920aa8af24f7778563a136e8737bc
liuqiang <joeliu1985@outlook.com>
2020-12-25 15:57:56 +0800

    read csv

---
+++ testrequests/testAPI.py
@@ -0,0 +1,21 @@
+import requests
+url="https://api.bilibili.com/x/click-interface/web/heartbeat"
+#Form data 格式
+postData = {
+    'aid': 35122164,
+    'cid': 107464593,
+    'bvid': 'BV1eb411P7tV',
+    'mid': 17636028,
+    'csrf':'b1382725b33244d5a1774390e1a14841',
+    'played_time':1815,
+    'real_played_time':1815,
+    'start_ts':1608880593,
+    'type':3,
+    'dt':2,
+    'play_type':0
+}
+
+response = requests.post(url,data=postData)
+print(response.json())
+
+
